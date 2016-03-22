173292bbc82e1fa5491c694bb6932565b0ebdcb9
rotem benjamin <rotemb1985@gmail.com>
2016-03-22 11:35:52 +0200

    add Course class

---
+++ Course.rb
@@ -0,0 +1,12 @@
+class Course
+	@id; @name; @teacher; @day; @room
+	
+	def initialize(id, name, teacher, day, room)
+		@id = id
+		@name = name 
+		@teacher = teacher 
+		@day = day 
+		@room = room
+	end
+	
+end
